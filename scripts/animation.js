d54e0dadf7be0679b609cb94ad2e6ac0a9a423d9
Luiz Eduardo <cunhaworkspace@gmail.com>
2024-05-22 17:19:09 -0400

    Getting the time cursor idle

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  const movingSprite = document.getElementById('myCanvas');
-
-  movingSprite.addEventListener('transitionstart', () => {
-    startAnimation();
-  });
-
-  movingSprite.addEventListener('transitionend', () => {
-    stopAnimation();
-  });
-
-  movingSprite.addEventListener('mousemove', (event) => {
-      const randomCoords = getRndInteger(getSizes());
-      performAction(movingSprite, randomCoords);
-    });
-  });
-
 const getSizes = () => {
   const body = document.querySelector('body');
   const bodyWidth = body.offsetWidth;
@@ -24,8 +6,8 @@ const getSizes = () => {
 };
 
 const getRndInteger = (array, min = 125) => {
-  const valueXCoord = Math.floor(Math.random() * (array[0] - min) ) + min;
-  const valueYCoord = Math.floor(Math.random() * (array[1] - min) ) + min;
+  const valueXCoord = Math.floor(Math.random() * (array[0] - min)) + min;
+  const valueYCoord = Math.floor(Math.random() * (array[1] - min)) + min;
   return [valueXCoord, valueYCoord];
 };
 
@@ -33,3 +15,34 @@ const performAction = (sprite, randomCoordArray) => {
   sprite.style.left = `${randomCoordArray[0]}px`;
   sprite.style.top = `${randomCoordArray[1]}px`;
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const movingSprite = document.getElementById('myCanvas');
+  const bodyQuery = document.querySelector('body');
+
+  let idleTimeout;
+
+  const resetIdleTimer = () => {
+    if (idleTimeout) {
+      clearTimeout(idleTimeout);
+    }
+    idleTimeout = setTimeout(timeElapsedIdle, 4000);
+  };
+
+  const timeElapsedIdle = () => {
+    console.log('Mouse has been idle for 4 seconds');
+  };
+
+  movingSprite.addEventListener('transitionstart', startAnimation);
+
+  movingSprite.addEventListener('transitionend', stopAnimation);
+
+  movingSprite.addEventListener('mousemove', (event) => {
+    const randomCoords = getRndInteger(getSizes());
+    performAction(movingSprite, randomCoords);
+  });
+
+  bodyQuery.addEventListener('mousemove', resetIdleTimer);
+
+  resetIdleTimer();
+});
